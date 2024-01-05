46d523cf235bc0272ece142daadb1a3df2a34b53
Aryan <aryanvedwal01@gmail.com>
2024-01-05 16:17:20 +0530

    some js changes

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     const menuToggleBtns = document.querySelectorAll('.menu-toggle-btn');
     const menu = document.querySelector('.menu');
+    const dropToggles = document.querySelectorAll('.drop-toggle');
 
     menuToggleBtns.forEach(function (btn) {
         btn.addEventListener('click', function () {
             menu.classList.toggle('open');
+        });
+    });
+
+    dropToggles.forEach(function (dropToggle) {
+        dropToggle.addEventListener('click', function () {
+            menu.classList.remove('open');
+            // Add additional logic or actions as needed
         });
     });
 });
